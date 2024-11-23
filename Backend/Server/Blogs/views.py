647b2558191795c836c71f45a804d2398a2121b5
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-11-23 12:19:58 +0330

    Blog view set added to views.py file that includes the list and retrieve method.

--- Backend/Server/Blogs/views.py
+++ Backend/Server/Blogs/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.status import *
 
-# Create your views here.
+from .models import Blog, BlogSection
+from .serializers import BlogSerializer, BlogSectionSerializer
+
+# ViewSet for handling Blog-related requests
+class BlogViewSet(ViewSet):
+    
+    # Method to handle GET requests for listing all blogs
+    def list(self, request):
+        # Retrieve all Blog instances from the database
+        blogs = Blog.objects.all()
+        
+        # Serialize the blog instances using the BlogSerializer
+        serializer = BlogSerializer(blogs, many=True)
+        
+        # Return the serialized data as a response
+        return Response(serializer.data)
+    
+    # Method to handle GET requests for retrieving a specific blog by its primary key (pk)
+    def retrieve(self, request, pk):
+        # Fetch the Blog instance or return a 404 error if not found
+        blog = get_object_or_404(Blog, pk=pk)
+        
+        # Serialize the specific blog instance
+        serializer = BlogSerializer(blog)
+        
+        # Return the serialized data as a response
+        return Response(serializer.data)
