519ed418d63db6038f83404cc0f202a37da35351
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-12-03 15:37:11 +0330

    ContactViewSet is added to the project.

--- Backend/Server/ContactUs/views.py
+++ Backend/Server/ContactUs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.viewsets import ViewSet
+from rest_framework.views import APIView, Response
+from rest_framework import status
+
+from . import models, serializers
+
+
+
+
+class ContactViewSet(ViewSet):
+    
+    def list(self, request):
+        queryset = models.Contact.objects.all()
+        serializer = serializers.ContactSerializer(instance=queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retreive(self, request, pk):
+        query = get_object_or_404(models.Contact, id=pk)
+        serializer = serializers.ContactSerializer(instance=query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
