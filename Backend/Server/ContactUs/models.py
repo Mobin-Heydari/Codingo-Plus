a85eb400d438e0cf150c563d7c6b9f277ee1662c
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-12-03 15:29:11 +0330

    Contact model is added.

--- Backend/Server/ContactUs/models.py
+++ Backend/Server/ContactUs/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
-# Create your models here.
+
+
+class Contact(models.Model):
+    
+    full_name = models.CharField(
+        max_length=250,
+        blank=True,
+        null=True
+    )
+    
+    phone = models.CharField(
+        max_length=12,
+        blank=True,
+        null=True
+    )
+    
+    email = models.EmailField(
+        validators=[EmailValidator],
+        blank=True,
+        null=True
+    )
+    
+    message = models.TextField(
+        blank=True,
+        null=True
+    )
+    
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta:
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
+
+    
+    def __str__(self):
+        return f"{self.full_name} {self.massage}"
