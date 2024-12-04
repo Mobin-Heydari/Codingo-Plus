a79e0a720d836bce9784eaeb242400dad47bb3ce
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-12-04 11:11:49 +0330

    Projects model is added to the Projects app.

--- Backend/Server/Projects/models.py
+++ Backend/Server/Projects/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Project(models.Model):
+    
+    class ProjectStatus(models.TextChoices):
+        IN_PROGRESS = 'In Progress', 'In Progress'
+        COMPLETED = 'Completed', 'Completed'
+        PENDING = 'Pending', 'Pending'
+        CANCELED = 'Canceled', 'Canceled'
+        
+    
+    title = models.CharField(max_length=200)
+    
+    description = models.TextField()
+    
+    image = models.ImageField(upload_to='Projects/Images/')
+    
+    url = models.URLField(blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    status = models.CharField(
+        max_length=20,
+        choices=ProjectStatus.choices,
+        default=ProjectStatus.PENDING
+    )
+    
+    
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+    
+    
+    def __str__(self):
+        return self.title
+    
