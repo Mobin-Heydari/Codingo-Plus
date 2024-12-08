20f0ed41ebd057e569082b93c3ec5da9fbe910df
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-12-08 13:32:48 +0330

    Screan and spacing is customized.

--- Frontend/client/tailwind.config.ts
+++ Frontend/client/tailwind.config.ts
@@ -6,6 +6,24 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
+  screens: {
+    'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '768',
+    // => @media (min-width: 768) { ... }
+
+    'lg': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'xl': '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
+  },
+
   theme: {
     extend: {
       colors: {
