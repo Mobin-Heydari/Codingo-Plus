cbdae14b96df0366dadad87629aaf5d646ff24a2
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-12-09 18:23:55 +0330

    Services section is added to the main page.

--- Frontend/client/src/app/page.tsx
+++ Frontend/client/src/app/page.tsx
@@ -4,35 +4,35 @@ import web_card from '../assets/imgs/home/web.jpg';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-evenly my-24">
+    <main className="flex flex-col justify-evenly">
 
-      <section className="flex flex-row max-lg:flex-col p-12 justify-around max-sm:p-2">
+      <section className="flex flex-row max-lg:flex-col p-8 justify-around max-sm:p-2 my-12">
 
         <div className="basis-2/5 flex justify-between flex-col">
 
-          <h2 className="text-3xl text-secondary p-5 my-4 rounded border-r-4 border-r-primary max-sm:text-2xl">تحقق رویاهای <span className="text-primary">دیجیتال</span> شما</h2>
+          <h2 className="text-3xl text-secondary p-5 my-4 rounded border-r-4 border-r-primary max-sm:text-2xl font-bold">تحقق رویاهای <span className="text-primary">دیجیتال</span> شما</h2>
           <p className="text-lg text-text p-6 my-4 rounded border-r-4 border-r-primary">
             در <strong className="font-bold text-secondary">مجموعه کدینگ و پلاس</strong>، ما با ترکیب خلاقیت و فناوری پیشرفته، راه‌حل‌هایی نوآورانه برای وب‌سایت‌ها و اپلیکیشن‌های شما ارائه می‌دهیم. ما به تحقق رؤیاهای دیجیتالی شما ایمان داریم و تلاش می‌کنیم تا هر پروژه را به یک داستان موفقیت‌آمیز تبدیل کنیم.
           </p>
           <p className="text-lg text-text p-6 my-4 rounded border-r-4 border-r-secondary">
-            <strong>مأموریت ما:</strong> خلق تجربه‌های دیجیتالی که نه تنها جذاب و کارآمد هستند بلکه تأثیری ماندگار بر روی کسب‌وکار شما می‌گذارند.
+            <strong className="font-bold">مأموریت ما:</strong> خلق تجربه‌های دیجیتالی که نه تنها جذاب و کارآمد هستند بلکه تأثیری ماندگار بر روی کسب‌وکار شما می‌گذارند.
           </p>
           <p className="text-lg text-text p-6 my-4 rounded border-r-4 border-r-primary">
             کدنیگو پلاس مجموعه ای از طراحان و توسعه دهندگانی با تجربه و مدرن است که در زمینه ی طراحی و توسعه وب اپلیکیشن و اپلیکیشن سال ها تجربه و تخصص جمع آوری کرده اند.
           </p>
 
           <div className="flex justify-evenly max-sm:flex-col text-center my-5 flex-wrap gap-4">
-            <p className="cursor-pointer p-4 text-sm text-text rounded-xl shadow-custom-light border-r-4 border-primary max-sm:my-3">بشتیبانی رایگان</p>
-            <p className="cursor-pointer p-4 text-sm text-text rounded-xl shadow-custom-light border-r-4 border-primary max-sm:my-3">طراحی اختصاصی</p>
-            <p className="cursor-pointer p-4 text-sm text-text rounded-xl shadow-custom-light border-r-4 border-secondary max-sm:my-3">کدنویسی بهینه</p>
-            <p className="cursor-pointer p-4 text-sm text-text rounded-xl shadow-custom-light border-r-4 border-primary max-sm:my-3">تکنولوژی های روز</p>
+            <p className="cursor-pointer p-4 text-sm text-text rounded-xl shadow-custom-light border-r-4 border-primary max-sm:my-3 font-bold">بشتیبانی رایگان</p>
+            <p className="cursor-pointer p-4 text-sm text-text rounded-xl shadow-custom-light border-r-4 border-primary max-sm:my-3 font-bold">طراحی اختصاصی</p>
+            <p className="cursor-pointer p-4 text-sm text-text rounded-xl shadow-custom-light border-r-4 border-secondary max-sm:my-3 font-bold">کدنویسی بهینه</p>
+            <p className="cursor-pointer p-4 text-sm text-text rounded-xl shadow-custom-light border-r-4 border-primary max-sm:my-3 font-bold">تکنولوژی های روز</p>
           </div>
 
           <div className="flex justify-evenly text-center my-5 max-sm:flex-col gap-4">
-            <a className="p-4 bg-secondary rounded-2xl w-2/4 text-white text-center shadow-custom-light hover:bg-primary hover:text-text primary-transitsion max-sm:w-full" href="">
+            <a className="p-4 bg-secondary rounded-2xl w-2/4 text-white text-center shadow-custom-light hover:bg-primary hover:text-text primary-transitsion max-sm:w-full font-bold" href="">
               دریافت مشاوره
             </a>
-            <a className="p-4 bg-primary rounded-2xl w-2/4 text-text text-center shadow-custom-light hover:bg-hover hover:text-white primary-transitsion max-sm:w-full" href="">
+            <a className="p-4 bg-primary rounded-2xl w-2/4 text-text text-center shadow-custom-light hover:bg-hover hover:text-white primary-transitsion max-sm:w-full font-bold" href="">
               نمونه کار ها
             </a>
           </div>
@@ -44,25 +44,72 @@ export default function Home() {
         
       </section>
 
-      {/* Our services part */}
-      <section className="flex justify-evenly flex-col gap-4">
+      {/* Services part */}
+      <section className="flex justify-evenly flex-col gap-4 p-8 max-sm:p-0 shadow-custom-light my-12">
 
-        <div>
-          <h3>کدینگو چه خدماتی به شما ارايه می دهد؟</h3>
-          <h4>طراحی و توسعه انواع بلتفرم ها و کسب و کار های آنلاین و راه اندازی آن ها در بستر وب و مبایل از تخصص متخصاصن کدینگو است</h4>
-        </div>
-
-        <div className="flex justify-evenly flex-wrap gap-5">
-        <div className="relative flex justify-evenly flex-col w-1/6 shadow-custom-dark rounded-3xl p-5 gap-5 hover:bg-orange-100 hover:w-1/5 primary-transitsion">
-          <div className="relative">
-            <Image src={web_card} alt="Codingo Plus" width={400} height={200} className="rounded-3xl max-w-full h-auto"/>
-            <div className="absolute inset-0 bg-white opacity-50 transition-opacity duration-300 hover:opacity-100"></div>
-            <div className="absolute inset-0 flex flex-col justify-center items-center text-center opacity-0 transition-opacity duration-300 hover:opacity-100">
-              <h5 className="text-xl text-secondary hover:text-primary primary-transitsion">طراحی و توسعه وب</h5>
-              <p className="text-sm text-justify p-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit corporis ducimus expedita! Voluptatum praesentium minima adipisci aliquam fugit ut nemo similique debitis unde cumque. Expedita necessitatibus at unde maiores quo aperiam aspernatur repellendus aliquid nam? Commodi obcaecati, a voluptatem accusamus consequatur velit culpa rerum. Minus ipsa similique sequi facilis. Natus.</p>
-            </div>
+        <div className="text-center text-text flex flex-col justify-between gap-3">
+          <div>
+            <h3 className="text-3xl text-secondary p-3 m-3 rounded border-r-4 border-r-secondary inline-block font-bold"><span className="text-primary">مجموعه کدینگو پلاس</span> چه خدماتی به شما ارايه می دهد؟</h3>
+          </div>
+          <div className="">
+            <h4 className="text-2xl p-3 m-2 font-bold rounded border-r-4 border-r-primary inline-block">طراحی و توسعه انواع بلتفرم ها و کسب و کار های آنلاین و راه اندازی آن ها در بستر وب و مبایل از تخصص متخصاصن کدینگو است</h4>
           </div>
         </div>
+
+        {/* card container */}
+        <div className="flex justify-evenly flex-wrap gap-x-2 gap-y-7 p-2">
+
+          {/* cards */}
+          <div className="flex justify-evenly flex-col gap-8 w-full sm:w-1/3 md:w-1/4 lg:w-1/5 shadow-custom-dark rounded-3xl p-2">
+            <div>
+              <Image src={web_card} alt="codingo" className="rounded-3xl max-w-full h-auto"/>
+            </div>
+            <div className="flex flex-col justify-evenly items-center text-center gap-4">
+              <a href="" className="rounded-md border-r-primary border-r-4 p-2">
+                <h5 className="text-xl text-secondary hover:text-primary primary-transitsion font-bold">طراحی و توسعه وب</h5>
+              </a>
+              <p className="text-sm text-justify p-2">texts here</p>
+            </div>
+          </div>
+
+          {/* Repeat the card div for additional cards */}
+          <div className="flex justify-evenly flex-col gap-8 w-full sm:w-1/3 md:w-1/4 lg:w-1/5 shadow-custom-dark rounded-3xl p-2">
+            <div>
+              <Image src={web_card} alt="codingo" className="rounded-3xl max-w-full h-auto"/>
+            </div>
+            <div className="flex flex-col justify-evenly items-center text-center gap-4">
+              <a href="" className="rounded-md border-r-primary border-r-4 p-2">
+                <h5 className="text-xl text-secondary hover:text-primary primary-transitsion font-bold">طراحی و توسعه وب</h5>
+              </a>
+              <p className="text-sm text-justify p-2">texts here</p>
+            </div>
+          </div>
+          {/* Repeat the card div for additional cards */}
+          <div className="flex justify-evenly flex-col gap-8 w-full sm:w-1/3 md:w-1/4 lg:w-1/5 shadow-custom-dark rounded-3xl p-2">
+            <div>
+              <Image src={web_card} alt="codingo" className="rounded-3xl max-w-full h-auto"/>
+            </div>
+            <div className="flex flex-col justify-evenly items-center text-center gap-4">
+              <a href="" className="rounded-md border-r-primary border-r-4 p-2">
+                <h5 className="text-xl text-secondary hover:text-primary primary-transitsion font-bold">طراحی و توسعه وب</h5>
+              </a>
+              <p className="text-sm text-justify p-2">texts here</p>
+            </div>
+          </div>
+          {/* Repeat the card div for additional cards */}
+          <div className="flex justify-evenly flex-col gap-8 w-full sm:w-1/3 md:w-1/4 lg:w-1/5 shadow-custom-dark rounded-3xl p-2">
+            <div>
+              <Image src={web_card} alt="codingo" className="rounded-3xl max-w-full h-auto"/>
+            </div>
+            <div className="flex flex-col justify-evenly items-center text-center gap-4">
+              <a href="" className="rounded-md border-r-primary border-r-4 p-2">
+                <h5 className="text-xl text-secondary hover:text-primary primary-transitsion font-bold">طراحی و توسعه وب</h5>
+              </a>
+              <p className="text-sm text-justify p-2">texts here</p>
+            </div>
+          </div>
+
+        {/* Add more cards as needed */}
         </div>
 
       </section>
