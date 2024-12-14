c4f23d08ae6d5012437e037ff7ba5a28fa08394f
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-12-14 21:55:30 +0330

    the new section is optimized and responsived but it has more to be complete.

--- Frontend/client/src/app/page.tsx
+++ Frontend/client/src/app/page.tsx
@@ -227,40 +227,51 @@ export default function Home() {
       </section> 
 
       {/* Why Codingo Section*/}
-      <section>
-        <div>
-          <Image src={why_codingo} alt="چرا کدینگو پلاس؟"/>
-        </div>
+      <section className="flex flex-row max-lg:flex-col p-8 justify-around max-sm:p-2 my-12">
 
-        <div>
-          <h5>چرا کدینگو پلاس؟</h5>
+        <div className="basis-3/5 flex justify-between flex-col">
 
-          <div>
+          <div className="text-center text-text flex flex-col justify-between gap-3">
+            <h5 className="text-3xl text-primary p-5 my-4 rounded border-r-4 border-r-primary max-sm:text-2xl font-bold">چرا کدینگو پلاس؟</h5>
+          </div>
+
+          <div className="text-center text-text flex flex-col justify-between gap-3">
             <h6><span>1.</span>کیفیت بالا</h6>
             <p>ما از آخرین استانداردهای سئو و تجربه کاربری برای طراحی وب‌سایت‌ های شما استفاده می‌کنیم که با تجارت و کسب و کار شما هماهنگ باشند.</p>
           </div>
 
-          <div>
+          <div className="text-center text-text flex flex-col justify-between gap-3">
             <h6><span>2.</span>طراحی خاص</h6>
             <p>پلتفرم شما باید علاوه بر عملکردها، زیبایی هم داشته باشد. ما با طراحی متفکرانه و خلاقانه، وب‌سایتی را ایجاد می‌کنیم که بازدیدکنندگان را ترغیب به کاوش در صفحات کند.</p>
           </div>
 
-          <div>
+          <div className="text-center text-text flex flex-col justify-between gap-3"> 
             <h6><span>3.</span>پشتیبانی رایگان</h6>
             <p>ما به شما پشتیبانی رایگان به مدت یک سال ارائه می‌دهیم. اگر سوالی دارید یا نیاز به راهنمایی دارید، همیشه در کنار شما هستیم.</p>
           </div>
 
-          <div>
+          <div className="text-center text-text flex flex-col justify-between gap-3">
             <h6><span>4.</span>تضمین عملکرد</h6>
             <p> قبل از راه‌اندازی نهایی، پلتفرم را تست و ارزیابی می‌کنیم تا اطمینان حاصل شود که همه عملکردها به درستی کار می‌کنند و تجربه کاربری بهینه است.</p>
           </div>
 
-          <div>
+          <div className="text-center text-text flex flex-col justify-between gap-3">
             <h6><span>5.</span>سرعت بالا</h6>
             <p>پلتفرم های ما بهینه‌شده‌اند تا سرعت بارگذاری صفحات به حداقل برسد. این امر به تجربه کاربری و همچنین بهبود سئو کمک می‌کند.</p>
           </div>
 
         </div>
+
+        <div className="">
+          <Image 
+            src={why_codingo} 
+            alt="چرا کدینگو پلاس؟" 
+            width={450} 
+            height={250} 
+            className="rounded-xl"
+          />
+        </div>
+
       </section>
 
       <ProjectsSection></ProjectsSection>
