98c522d131591811441b053e799d90d55dde8a1d
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-12-15 14:12:37 +0330

    Faq model is added and migrated.

--- Backend/Server/FAQs/models.py
+++ Backend/Server/FAQs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255, verbose_name="سوال")
+    answer = models.TextField(verbose_name="جواب")
+    
+    class Meta:
+        verbose_name = 'سوال متداول'
+        verbose_name_plural = 'سوالات متداول'
+    
+    def __str__(self):
+        return self.answer
+    
