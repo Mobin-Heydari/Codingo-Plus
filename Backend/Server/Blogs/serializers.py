633abeba3bd6cefeac54a806030ccfc1eca8c2e3
Mobin Heydari <mobinheydari.developer@gmail.com>
2024-12-18 23:59:12 +0330

    CategorySerializer is optimized.

--- Backend/Server/Blogs/serializers.py
+++ Backend/Server/Blogs/serializers.py
@@ -58,6 +58,22 @@ class BlogSectionSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
     
+    image_url = serializers.SerializerMethodField()
+    
     class Meta:
         model = Category  # Specify the model to serialize
-        fields = '__all__'  # Include all fields from the Category model
+        fields = ['id', 'category', 'slug', 'image_url']
+        
+    def get_image_url(self, obj):
+        # Convert the image field to a string
+        image = str(obj.image)
+        
+        # Determine the host URL based on ALLOWED_HOSTS
+        if ALLOWED_HOSTS == []:
+            path_host = "http://127.0.0.1:8000"  # Localhost if no hosts are allowed
+        else:
+            path_host = ALLOWED_HOSTS[0]  # Use the first allowed host
+        
+        # Construct the full image URL
+        image_url = f"{path_host}/media/{image}"
+        return image_url  # Return the constructed image URL
